d210ca0239eab4683a156b06fa8b35068291b996
GOver1519 <124514705+GOver1519@users.noreply.github.com>
2023-04-17 21:40:07 +0530

    Add files via upload

---
+++ Dijkstra's algorithm.py
@@ -0,0 +1,31 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {vertex:float('infinity') for vertex in graph}
+    distances[start] = 0
+    
+    pq = [(0, start)]
+
+    while pq:
+        current_distance, current_vertex = heapq.heappop(pq)
+        
+
+        if current_distance > distances[current_vertex]:
+            continue
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+
+    return distances
+
+graph = {
+    'A': {'B': 5, 'C': 3},
+    'B': {},
+    'C': {}
+}
+distances = dijkstra(graph, 'A')
+print(distances)
+
